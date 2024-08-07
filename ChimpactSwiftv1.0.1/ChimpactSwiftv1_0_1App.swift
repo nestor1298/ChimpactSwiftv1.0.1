31fa17848a62f7e59fde7a4cfb0a1c9f245c6c57
Nestor Aldrete <ldrete.nestor@gmail.com>
2024-08-07 13:22:41 -0600

    Initial Commit

---
+++ ChimpactSwiftv1.0.1/ChimpactSwiftv1_0_1App.swift
@@ -0,0 +1,17 @@
+//
+//  ChimpactSwiftv1_0_1App.swift
+//  ChimpactSwiftv1.0.1
+//
+//  Created by Néstor Aldrete Ochoa on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ChimpactSwiftv1_0_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChimpactSwiftv1.0.1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChimpactSwiftv1.0.1
+//
+//  Created by Néstor Aldrete Ochoa on 07/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
